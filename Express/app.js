da051bf06ec13c8025341722df90d82fd5fa2394
Alwaysram <rc4663128@gmail.com>
2023-05-25 16:10:16 +0530

    introduction

---
+++ Express/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app=express();
+app.use((req,res,next)=>{
+    console.log('In the middleWare');
+    next();
+});
+app.use((req,res,next)=>{
+    console.log('In the another middleWare');
+    res.send('<h1>Hello from Express!</h1>');
+});
+app.listen(3000);
